4fa52032d6c069061721033cf5b31887682de403
Nandkishor <nrr.tvans@gmail.com>
2023-02-20 23:20:48 +0530

    Chorchithi- increase readability

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,18 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
-        int flag = 1;
+        var flag = 1;
 
-        System.out.println("Welcome to ChorChiththi");
-        System.out.println("Enter players Name:-");
+        System.out.println("Welcome to ChorChitthi");
+        System.out.println("Enter name of players:-");
         for (int i = 0; i < 4; i++) {
-            System.out.println("Player " + (i + 1) + ": ");
+            System.out.print("Player " + (i + 1) + ": ");
             players[i] = sc.next();
         }
 
         System.out.println("\n---***---\n");
 
-        System.out.println("Enter number of match you wont play.");
+        System.out.println("Enter number of rounds you wont play.");
         match = sc.nextInt();
         score = new int[match][4];
 
@@ -30,10 +30,11 @@ public class Main {
         while (remainMatch > 0){
             System.out.println("\n-----------\n");
             System.out.println("You have " + remainMatch + " chance(s).");
-            System.out.println("Through(Enter 1): ");
+            System.out.println("Draw(Enter any key): ");
 
-            flag = sc.nextInt();
-            if (flag == 1)
+//            flag = sc.nextInt();
+            flag = sc.next().charAt(0);
+            if (flag > 0)
                 play(match - remainMatch);
             else
                 continue;
@@ -45,10 +46,10 @@ public class Main {
 
     private static void resultBoard() {
         System.out.println("\n---***---\n");
-        System.out.println("RESULT BOARD\n");
+        System.out.println("SCORE BOARD\n");
 
         System.out.println("__________________________________________________________");
-        System.out.printf("|%11s  | %7s  | %7s | %7s  | %7s |\n","|Game ", players[0],  players[1], players[2],  players[3]);
+        System.out.printf("|%12s |%9s |%9s |%9s |%9s |\n", "|Game", players[0], players[1], players[2], players[3]);
         System.out.println("|_________________________________________________________|");
         for (int i = 0; i < match; i++) {
             System.out.printf("|%s |%5d |%9d |%9d |%9d |%9d |\n","Score", (i + 1), score[i][0], score[i][1], score[i][2], score[i][3]);
@@ -65,18 +66,16 @@ public class Main {
     }
 
     private static void play(int game) {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
 
-        String[] chit = {"", ""};
+        String[] chit;
         Random random = new Random();
-        Set set = new HashSet();
+        Set<Integer> set = new HashSet<>();
         int n;
         int chorIndex = 0;
         int mantriIndex = 0;
         int rajaIndex = 0;
-        int chorPata = 0;
-        int sipaiIndex = 0;
+        int chorPata;
+//        int sipaiIndex = 0;
         String[] characterSequence = new String[4];
 
         for (int i = 0; i < 4; i++) {
@@ -95,8 +94,8 @@ public class Main {
                     mantriIndex = j;
                 else if (score[game][j] == 1000)
                     rajaIndex = j;
-                if (score[game][j] == 300)
-                    sipaiIndex = j;
+//                if (score[game][j] == 300)
+//                    sipaiIndex = j;
             }
 
             if (score[game][i] == 0 || score[game][i] == 300)
@@ -106,25 +105,26 @@ public class Main {
             characterSequence[i] = chit[0];
         }
 
-        System.out.println("\nRaja(" + players[rajaIndex] +")" + " dono mese chor kon?");
+        System.out.println("\n " + players[rajaIndex] + ": Chor/Sipai ka patalagao");
         chorPata = sc.nextInt() - 1;
-        System.out.println("Mantri(" + players[mantriIndex] +") " + players[chorPata] + " he janab.");
+        System.out.println(players[mantriIndex] + ": " + players[chorPata] + " he.");
 
         if ((chorIndex == chorPata)){
-            System.out.println("\nSahi javab!");
+            System.out.println("\nSahi jawab!");
 
             for (int i = 0; i < 4; i++)
                 System.out.println((i + 1) + ": " + players[i] + ": " + characterSequence[i] + " = " + score[game][i]);
         }
-        else if (chorPata  == chorIndex || chorPata == sipaiIndex){
-            System.out.println("\nGalat javab!");
+//        else if (chorPata == sipaiIndex){
+        else {
+            System.out.println("\nGalat jawab!");
             swap (mantriIndex, chorIndex, game);
 
             for (int i = 0; i < 4; i++)
                 System.out.println((i + 1) + ": " + players[i] + ": " + characterSequence[i] + " = " + score[game][i]);
         }
-        else
-            System.out.println("\nYe ky he...");
+       /* else
+            System.out.println("\nYe ky he...");*/
 //        System.out.println("mantriIndex " + mantriIndex + "\nchorIndex " + chorIndex);
     }
 
@@ -135,7 +135,6 @@ public class Main {
     }
 
     private static String[] getChith(int index) {
-        String[] chith = {character[index], String.valueOf(value[index])};
-        return chith;
+        return new String[]{character[index], String.valueOf(value[index])};
     }
 }
